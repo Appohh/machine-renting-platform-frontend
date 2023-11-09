6d764b63bda51de4e404cdd197534cce535ba08e
Appie <alpay2001@live.nl>
2023-11-10 00:07:53 +0100

    home design

--- rental-tycoon-react/src/App.jsx
+++ rental-tycoon-react/src/App.jsx
@@ -4,6 +4,7 @@ import AddRent from './pages/AddRent';
 import SearchPage from "./pages/SearchPage";
 import '../src/App.css';
 import CreateMachine from './pages/CreateMachine';
+import logo from './assets/react.svg'
 
 
 function App() {
@@ -11,13 +12,21 @@ function App() {
     <>
       <div className='main-wrap'>
         <Router>
-          <div className='nav'></div>
+          <div className='nav'>
+            <li>
+              <ul><a href="/">Home</a></ul>
+              <ul><a href="/">Catalog</a></ul>
+              <ul><img src={logo} alt="logo"></img></ul>
+              <ul><a href="/">About</a></ul>
+              <ul><a href="/">Contact</a></ul>
+            </li>
+          </div>
           <div className='content'>
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/addRent" element={<AddRent />} />
-              <Route path="/addMachine" element={<CreateMachine/>} />
-              <Route path="/search" element={<SearchPage/>}/>
+              <Route path="/addMachine" element={<CreateMachine />} />
+              <Route path="/search" element={<SearchPage />} />
             </Routes>
           </div>
         </Router>
--- rental-tycoon-react/src/pages/Home.jsx
+++ rental-tycoon-react/src/pages/Home.jsx
@@ -1,11 +1,34 @@
 import { useState } from 'react'
+import herobanner from '../assets/images/rental-services.jpg'
 
 
 function Home() {
 
   return (
     <>
-        <h1>Home</h1>
+      <section className='hero'>
+        <div className='hero-content'>
+          <h1>Your partner for safe, productive & sustainable machines.</h1>
+          <img src={herobanner} alt='hero'></img>
+        </div>
+
+        <form className='search-container'>
+          <input className='search-bar' placeholder='Search for machines...' type="text" />
+        </form>
+      </section>
+      <section className='section-blank section-categories'>
+        <h2>Categories</h2>
+        <div className='categories-container'>
+          <div className='category'></div>
+          <div className='category'></div>
+          <div className='category'></div>
+          <div className='category'></div>
+          <div className='category'></div>
+          <div className='category'></div>
+          <div className='category'></div>
+          <div className='category'></div>
+        </div>
+      </section>
     </>
   )
 }
