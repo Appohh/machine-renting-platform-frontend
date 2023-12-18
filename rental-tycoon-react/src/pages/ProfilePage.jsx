db1bf10e9dcd782f72d50f6cfbbae6602ce0d37d
solaalec <sola.alec@gmail.com>
2023-12-18 12:40:54 +0100

    update user

--- rental-tycoon-react/src/pages/ProfilePage.jsx
+++ rental-tycoon-react/src/pages/ProfilePage.jsx
@@ -9,6 +9,8 @@ function ProfilePage() {
 
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
+  const [editMode, setEditMode] = useState(false);
+
   const token = LocalStorageService.get();
   const { decodedToken } = useJwt(token || "");
 
@@ -26,7 +28,12 @@ function ProfilePage() {
     }
   }, [decodedToken]);
 
-
+  const handleInputChange = (e, field) => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      [field]: e.target.value,
+    }));
+  };
   const handleUpdatePage = () => {
     navigate("/UpdateProfilePage")
   }
@@ -36,7 +43,25 @@ function ProfilePage() {
       navigate(`/History/${user.id}`);
     }
   };
-
+  const handleSaveChanges = async () => {
+    // Implement the logic to save changes using the UserService
+    try {
+      await UserService.updateUserDetails(
+        user.id,
+        user.firstName,
+        user.lastName,
+        user.address,
+        user.city,
+        user.email,
+        user.phone
+      );
+      console.log("Success changes");
+      setEditMode(false); // Exit edit mode after saving changes
+    } catch (error) {
+      console.error("Error updating user details:", error);
+      // Handle error if needed
+    }
+  };
   return (
     <div className='account-content'>
       <div className='account-sidebar'>
@@ -50,37 +75,50 @@ function ProfilePage() {
       <div className="account-container">
         <h1>Account information</h1>
         {user && (
-          <div className='profile-info-container'>
-            <div className='profile-field'>
-              <label>First Name:</label>
-              <input type="text" value={user.firstName} readOnly />
-            </div>
+          <>
+            <div className='profile-info-container'>
+              <div className='profile-field'>
+                <label>First Name:</label>
+                <input type="text" value={user.firstName} readOnly={!editMode} onChange={(e) => handleInputChange(e, 'firstName')} />
+              </div>
 
-            <div className='profile-field'>
-              <label>Last Name:</label>
-              <input type="text" value={user.lastName} readOnly />
-            </div>
+              <div className='profile-field'>
+                <label>Last Name:</label>
+                <input type="text" value={user.lastName} readOnly={!editMode} onChange={(e) => handleInputChange(e, 'lastName')}/>
+              </div>
 
-            <div className='profile-field'>
-              <label>Address:</label>
-              <input type="text" value={user.address} readOnly />
-            </div>
+              <div className='profile-field'>
+                <label>Address:</label>
+                <input type="text" value={user.address} readOnly={!editMode} onChange={(e) => handleInputChange(e, 'address')} />
+              </div>
 
-            <div className='profile-field'>
-              <label>City:</label>
-              <input type="text" value={user.city} readOnly />
-            </div>
+              <div className='profile-field'>
+                <label>City:</label>
+                <input type="text" value={user.city} readOnly={!editMode} onChange={(e) => handleInputChange(e, 'city')} />
+              </div>
 
-            <div className='profile-field'>
-              <label>Email:</label>
-              <input type="text" value={user.email} readOnly />
-            </div>
+              <div className='profile-field'>
+                <label>Email:</label>
+                <input type="text" value={user.email} readOnly={!editMode} onChange={(e) => handleInputChange(e, 'email')} />
+              </div>
 
-            <div className='profile-field'>
-              <label>Phone:</label>
-              <input type="text" value={user.phone} readOnly />
+              <div className='profile-field'>
+                <label>Phone:</label>
+                <input type="text" value={user.phone} readOnly={!editMode} onChange={(e) => handleInputChange(e, 'phone')}/>
+              </div>
             </div>
-          </div>
+            {!editMode &&
+              <button onClick={() => setEditMode(true)}>Edit</button>
+            }
+            {editMode &&
+              <button onClick={() => setEditMode(false)}>Cancel Edit</button>
+            }
+            {editMode &&
+              <button onClick={handleSaveChanges}>Update</button>
+            }
+
+
+          </>
         )}
       </div>
     </div>
