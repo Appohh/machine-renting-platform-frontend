94e10eaac284a283c630a3fcb4c5baec1cb0d931
lvnchboxx <rangga.aulia@icloud.com>
2024-01-25 23:48:09 +0100

    About page

--- rental-tycoon-react/src/pages/AboutPage.jsx
+++ rental-tycoon-react/src/pages/AboutPage.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import './AboutPage.css';
 
 function AboutPage() {
   return (
-    <div>
-      <p>About Us</p>
-      <p>Welcome to our website! We are dedicated to providing you with the best services/products.</p>
-      <p>At our rental company, we specialize in providing top-quality machinery for various industries. 
-        Whether you're in construction, agriculture, or manufacturing, we have the equipment you need to get the job done efficiently. 
-        Our extensive inventory includes excavators, bulldozers, tractors, and much more. We take pride in offering flexible rental options, 
-        competitive pricing, and exceptional customer service to ensure your satisfaction. 
-        Contact us today to learn more about how we can support your projects!</p>
+    <div className="about-container">
+      <h1>About Us</h1>
+      <div className="about-content">
+        <p>Welcome to our website! We are dedicated to providing you with the best services/products.</p>
+        <p>
+          At our rental company, we specialize in providing top-quality machinery for various industries.
+          Whether you're in construction, agriculture, or manufacturing, we have the equipment you need to get the job done efficiently.
+        </p>
+        <p>
+          Our extensive inventory includes excavators, bulldozers, tractors, and much more. We take pride in offering flexible rental options,
+          competitive pricing, and exceptional customer service to ensure your satisfaction.
+        </p>
+        <p>Contact us today to learn more about how we can support your projects!</p>
+      </div>
     </div>
   );
 }
